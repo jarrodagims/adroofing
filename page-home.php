a31e9f1264d616e4f80bb327a41906c834c53df7
Jarrod Medrano <jmedran@gmail.com>
2018-11-21 16:57:57 -0700

    homepage updates

--- page-home.php
+++ page-home.php
@@ -22,7 +22,7 @@
             <h2>Repairs, Installations, & More</h2>
 
             <a href="<?= SITEURL ?>/contact-us/">
-                <button class="btn btn-primary">SCHEDULE AN APPOINTMENT</button>
+                <button class="btn btn-outline">SCHEDULE AN APPOINTMENT</button>
             </a>
 
             <a href="<?= SITEURL ?>/contact-us/">
@@ -36,187 +36,149 @@
 </section>
     </div> <!-- end home-bg -->
     <section class="icon-row">
-        <div class="container">
+        <div class="container-fluid">
             <?php echo get_template_part('template-parts/icon', 'row'); ?>
         </div>
     </section>
+
+    <section class="ribbon">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <p>GUTTERS & REMODELING</p>
+
+                    <em>
+                        Call Us To Get An <span class="brand">Estimate</span>
+                    </em>
+                </div>
+            </div>
+        </div>
+    </section>
+
 <section class="home-module-3">
     <div class="container">
         <div class="row flex-row">
+            <div class="col-md-6 flex-col bg-side">
+                <img class="visible-md visible-lg" src="<?=IMGURL?>years.png" alt="14 Years" />
+            </div>
             <div class="col-md-6 flex-col">
                 <div class="extra-padding">
-                    <h2><ul class="expand" id="expand">
-                            <li>We’re proud to be the best auto body shop in El Paso.</li>
-                            <li>Top-rated auto collision services for drivers.</li>
-                            <li>We’re happy to be highly reviewed by our customers!</li>
-                            <li>We are a local team that is dedicated to serving the community.</li>
-                            <li>Make us your go-to auto body shop company.</li>
-                            <li>Give us a call for information on quotes.</li>
-                            <li>We work quickly and can provide you with time estimates so that you can plan accordingly.
-                            </li>
-                            <li>Our team is here for all of your car repair needs.</li>
-                            <li>In need of dent repair? We’re the team to choose!</li>
-                            <li>Elite Auto Collision Center is here for you when you need us most.</li>
-                        </ul></h2>
+<!--                    <h2><ul class="expand" id="expand">-->
+<!--                            <li>We’re proud to be the best auto body shop in El Paso.</li>-->
+<!--                            <li>Top-rated auto collision services for drivers.</li>-->
+<!--                            <li>We’re happy to be highly reviewed by our customers!</li>-->
+<!--                            <li>We are a local team that is dedicated to serving the community.</li>-->
+<!--                            <li>Make us your go-to auto body shop company.</li>-->
+<!--                            <li>Give us a call for information on quotes.</li>-->
+<!--                            <li>We work quickly and can provide you with time estimates so that you can plan accordingly.-->
+<!--                            </li>-->
+<!--                            <li>Our team is here for all of your car repair needs.</li>-->
+<!--                            <li>In need of dent repair? We’re the team to choose!</li>-->
+<!--                            <li>Elite Auto Collision Center is here for you when you need us most.</li>-->
+<!--                        </ul></h2>-->
 
-                    <p class="blurb">If your car has suffered through an accident or collision, it’s time to seek repair services from our team of dedicated professionals.</p>
-                    <h5>Meet The Team <span class="arrow">&gt;</span></h5>
-                    <p>Elite Auto Collision Center is much more than your standard, run-of-the-mill auto body shop. Quality is our priority and we’ll stop at nothing to provide the best service possible. We work with all insurance companies so you won’t have to worry about anything! In fact, we use the same CCC system used by large insurance companies so your car’s repairs can go off without a hitch. So, if you’re ready to restore your car to its former, pre-collision glory, contact Elite Auto Collision Center!
+                    <p class="blurb"><span class="brand">A D Roofing</span> is a locally owned and operated <span class="brand">roofing company</span> in El Paso, Texas with over fourteen years of experience.</p>
+
+                    <p>A D Roofing is a locally owned and operated roofing company in El Paso, Texas with over fourteen years of experience. We have serviced and worked on every kind of roofing scenario possible including full installations, replacements, routine maintenance, and storm and wind damage.
                     </p>
-
-                    <p>Not only do we restore damaged vehicles to pristine condition, but we also offer general mechanic and dent removal services. If your car won’t start or if you need an oil change, we can help. We also offer 24/7 emergency service and towing so you’ll never have to be stranded ever again!</p>
                 </div>
-            </div>
-            <div class="col-md-6 flex-col bg-side">
-                
             </div>
         </div>
     </div>
-    <img class="visible-sm visible-xs" src="<?=IMGURL?>bg-side-sm.jpg" alt="Auto Insurance" />
+    <img class="visible-sm visible-xs" src="<?=IMGURL?>years.png" alt="14 Years" />
 </section>
 
+<section class="home-module-5">
+    <div class="container">
+        <div class="flex-row row">
+            <div class="col-md-6 flex-col">
+                <div class="extra-padding">
+                    <h2>Our Specialty</h2>
+
+                    <p class="blurb">A D Roofing specializes in all things roofing. We know that people are often overwhelmed when it comes to dealing with their roof. Many times the damage has snuck up on them or suddenly appears after a storm.</p>
+
+                    <p>Without proper maintenance and inspections, roof damage can also occur without visible indications until it’s too late. Other times, the issue is complex and somewhat catastrophic. That’s why we’re here. We spend every day working around the city of El Paso to help residents resolve their roofing issues. Our experienced roofers perform all roofing repairs and maintenance needed to keep your roof secure. We perform everything from roof replacement, coating, leak repairs, inspections, gutter fixes, and more!</p>
+
+                    <p>While we are lucky to have great weather in El Paso, having approximately 290 days of sunlight a year and moderate rain means specific maintenance and care for your roof. You don’t need much rain to cause damage to a roof. All it takes is a small amount of moisture breaking through a badly installed roof or compromised shingles. Moisture then seeps into your walls and begins to do structural damage to your home. This type of damage is generally caused by a small leak, bad flashing, and broken shingles.</p>
+
+                    <p>Even when a storm doesn’t produce hail or rain, wind and debris can cause damage over time. El Paso is known to have some pretty heavy wind storms. Over time, this can cause damage to your roof or house.</p>
+                </div>
+            </div>
+
+
+            <div class="col-md-6 bg-construction flex-col">
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="home-module-10">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="extra-padding">
+                        <h2>We Service the Entire El Paso Area</h2>
+
+
+                        <p>A D Roofing has been working the entire El Paso area for more than a decade. We have seen this city grow and have helped many people take care of their roofing needs. We are located on the east side of town on 6455 Hiller, Suite 10. While our offices are here, we work all throughout the city. Upon our appointment, we will come to your house and give you a free estimate. Whether you’re looking to replace the entire roof or simply perform routine maintenance. </p>
+
+                        <a href=""><button class="btn btn-outline">SCHEDULE AN APPOINTMENT</button></a>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
 
 <section class="home-module-4">
     <div class="container">
         <div class="row">
-            <div class="col-xs-12">
-                <ul class="logos">
-                    <li>
-                        <img src="<?=IMGURL?>geico.svg" alt="Geico" />
-                    </li>
-                    <li>
-                        <img src="<?=IMGURL?>progressive.svg" alt="Progressive" />
-                    </li>
-                    <li>
-                        <img src="<?=IMGURL?>allstate.svg" alt="AllState" />
-                    </li>
-                    <li>
-                        <img src="<?=IMGURL?>state-farm.svg" alt="State Farm" />
-                    </li>
-                    <li>
-                        <img src="<?=IMGURL?>usaa.svg" alt="USAA" class="usaa" />
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="ribbon">
-    <div class="extra-padding"><p>We will work with any insurance agencies not listed*</p></div>
-</section>
-
-
-<section class="home-module-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6"></div>
             <div class="col-md-6">
-                <div class="extra-padding"><p><strong>We Take Care of Everything, Literally!</strong></p>
 
-                <p>The journey towards completely repairing a vehicle after a collision may seem long and arduous but when you team up with us, you’ll have nothing to worry about! This is because we will take care of everything, from towing your car and speaking to your insurance company to repairing and repainting your vehicle so it will look good as new.</p>
+            </div>
+            <div class="col-md-6">
+                <h2>The Importance of Maintenance</h2>
 
-                 <p>Since we use the CCC system, we’ll be able to repair your vehicle with the exact parts required. We offer much more than collision repair; it’s our goal to offer high-quality service to drivers in the El Paso area. Choose Elite Auto Collision Center as your #1 auto body shop!</p></div>
+                <p>We stress the importance of maintaining and servicing your roof with regularity. To ensure there are no issues, we offer regular maintenance service and inspections. Our roofers will look for any trouble spots and if there are any, we will fix and stop the problem from becoming a bigger (and more expensive) issue.</p>
             </div>
         </div>
     </div>
+
 </section>
 
 <section class="home-module-6">
     <div class="container">
-        <div class="row flex-row">
-            <div class="flex-col">
-                <div class="extra-padding"><h2>Free towing assistance <span class="brand"><strong>AVAILABLE 24/7</strong></span></h2></div>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="<?=IMGURL?>bg-roof.jpg" alt="Roof" />
             </div>
-            <div class="flex-col">
-                <a href="<?=SITEURL?>/contact-us/">
-                    <button class="btn btn-primary">CALL US TODAY</button>
-                </a>
+            <div class="col-md-6">
+                <h2>Our Values and Our Promise</h2>
+
+                <h3>We strive for quality, value, and honesty.</h3>
+
+                <p>There are dozens of roofing companies in El Paso. So what sets A D Roofing apart? We take great care in doing the job right from the start. Many companies do a sloppy job, cut corners, and sacrifice quality for expediency. This often manifests in roof leaks, broken shingles, or damage soon after the work is done. Lazy roofers will do a haphazard job to collect their check and then disappear. You might not notice until the damage has spread and then becomes a hassle to deal with.</p>
+
+                <p>At A D Roofing, we are detail oriented and meticulous because we value quality. Our goal is longevity for your roof. We only employ qualified and experienced roofers who are proficient and knowledgeable in this line of work.</p>
+
+                <p>We are confident in all of our roof experts’ performance. That’s why we offer a 100% satisfaction guarantee and a ten-year labor warranty. It’s also why we do payment upon completion of our residential work. We want you to be satisfied and feel that the work you paid for was completed in a thorough and professional way.</p>
             </div>
         </div>
     </div>
 </section>
-<div class="flex-order">
-    <section class="home-module-7">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12">
-                    <div class="banner">
-                        <div class="flex-row">
-                            <div class="flex-col hidden-sm hidden-xs">
-                                <h2><span>We Offer</span><br />
-                                    <strong>MILITARY DISCOUNTS</strong></h2>
-                                <a href="<?=SITEURL?>/contact-us/"><button class="btn btn-primary">LEARN MORE</button></a>
-                            </div>
-                            <div class="flex-col banner-middle">
-                                <img class="visible-sm visible-xs" src="<?=IMGURL?>bg-family-sm.jpg" alt="Military Discounts" />
-                            </div>
-                            <div class="flex-col badge-col">
-                                <h2 class="visible-xs visible-sm"><span>We Offer</span><br />
-                                    <strong>MILITARY DISCOUNTS</strong></h2>
-                                <ul>
-                                    <li><img src="<?=IMGURL?>badge-airforce.png" alt="" class="military-badge"></li>
-                                    <li><img src="<?=IMGURL?>badge-marines.png" alt="" class="military-badge"></li>
-                                    <li><img src="<?=IMGURL?>badge-navy.png" alt="" class="military-badge"></li>
-                                    <li><img src="<?=IMGURL?>badge-army.png" alt="" class="military-badge"></li>
-                                </ul>
-                                <a class="visible-xs visible-sm" href="<?=SITEURL?>/contact-us/"><button class="btn btn-primary">LEARN MORE</button></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 
-    <section class="home-module-8">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2>What Our Auto Body Shop Has to Offer</h2>
 
-                    <p>Elite Auto Collision Center stands apart from the rest. We provide all-encompassing service so you won’t have to worry. Whether you got in a wreck, require mechanical assistance, or you need to get rid of a few dents, we can help! Take a look at what we have to offer:</p>
-                </div>
-            </div>
-
-            <div class="row bg">
-                <div class="col-md-6">
-                    <ul class="icon-row">
-                        <li class="flex-col"><?php get_template_part('/template-parts/icons/icon', '1'); ?> <div class="title">Free towing service</div></li>
-                        <li class="flex-col"><?php get_template_part('/template-parts/icons/icon', '2'); ?> <div class="title">Available 24/7</div></li>
-                        <li class="flex-col"><?php get_template_part('/template-parts/icons/icon', '3'); ?>  <div class="title">Uses the CCC insurance estimate system which achieves more accurate estimates</div>
-                        </li>
-                        <li class="flex-col"><?php get_template_part('/template-parts/icons/icon', '4'); ?> <div class="title">High-quality service, work, and standards</div></li>
-                        <li class="flex-col"><?php get_template_part('/template-parts/icons/icon', '5'); ?>  <div class="title">Military discounts available</div></li>
-                    </ul>
-                </div>
-                <div class="col-md-6">
-                    <ul class="icon-row">
-                        <li class="flex-col"><?php get_template_part('/template-parts/icons/icon', '6'); ?> <div class="title">Free full inspection</div></li>
-                        <li class="flex-col"><?php get_template_part('/template-parts/icons/icon', '7'); ?> <div class="title">Works with all insurance companies</div></li>
-                        <li class="flex-col"><?php get_template_part('/template-parts/icons/icon', '8'); ?> <div class="title">Works with the deductible</div></li>
-                        <li class="flex-col"><?php get_template_part('/template-parts/icons/icon', '9'); ?> <div class="title">Special rates for Government Agencies & Law Enforcement</div></li>
-                        <li class="flex-col"><?php get_template_part('/template-parts/icons/icon', '10'); ?> <div class="title">We use only the best Sherwin-Williams paint to finish the job</div></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </section>
-</div>
-<section class="home-module-10">
+<section class="home-module-7">
     <div class="container">
-
         <div class="row">
-            <div class="col-md-12">
-                <div class="extra-padding">
-                <h2>We’re Ready to Help - Contact Us Today</h2>
-
-                   <p><strong>If you’ve been in a collision, contact Elite Auto Collision Center today.</strong></p>
-
-                    <p>We understand how tedious the entire repair process may appear. For this reason, we’re devoted to making the process as easy and effortless so you won’t have to worry about anything! Let our team take care of everything. In no time, your vehicle will be back on the road. Contact us today to learn more about what we have to offer!</p>
-               
-               </div>
-
+            <div class="col-md-6">
+            </div>
+            <div class="col-md-6">
+                <h2>About Our Guarantee & Discounts</h2>
+                <p>We are confident in our services and roofers. For all residential roofing jobs, we offer 100% upon completion. This means you don’t have to pay us until you see the final result of our labor. At the same time, we offer a ten-year warranty labor guarantee on commercial and residential jobs. </p>
             </div>
         </div>
     </div>
--- template-parts/icon-row.php
+++ template-parts/icon-row.php
@@ -1,28 +1,31 @@
 <ul class="flex-row">
     <li class="flex-col">
         <a href="<?= SITEURL; ?>/services/auto-body-repair-el-paso/">
-            <?php get_template_part('template-parts/icons/icon', '01'); ?>
-            Body Repair
+            Replacement
         </a>
     </li>
     <li class="flex-col">
         <a href="<?= SITEURL; ?>/services/transmission-repair-el-paso/">
-            <?php get_template_part('template-parts/icons/icon', '02'); ?>
-            Transmission Repair</a>
+            Repair</a>
     </li>
     <li class="flex-col">
         <a href="<?= SITEURL; ?>/services/auto-detail-el-paso/">
-            <?php get_template_part('template-parts/icons/icon', '03'); ?>
-            Auto Detailing</a>
+            Leak Repair</a>
     </li>
     <li class="flex-col">
         <a href="<?= SITEURL; ?>/services/paintless-dent-repair-el-paso/">
-            <?php get_template_part('template-parts/icons/icon', '04'); ?>
-            Paintless Dent Repair</a></li>
+            Coating</a></li>
     <li class="flex-col">
         <a href="<?= SITEURL; ?>/services/hail-damage-repair-el-paso/">
-            <?php get_template_part('template-parts/icons/icon', '05'); ?>
-            Hail Damage Repair</a>
+            Inspections</a>
+    </li>
+
+    <li class="flex-col">
+        <a href="<?= SITEURL; ?>/services/hail-damage-repair-el-paso/">Shingles</a>
+    </li>
+
+    <li class="flex-col">
+        <a href="<?= SITEURL; ?>/services/hail-damage-repair-el-paso/">Commercial Roofing</a>
     </li>
     <li class="flex-col contact-button">
        <a href="<?=SITEURL?>/contact-us/">Call Us Today</a>
